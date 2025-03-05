b1ffd79b064249494baa6be62b2f6ee0e8376b07
Ygor Alejandro <ygorhernadezbr@gmail.com>
2025-03-05 12:17:50 -0300

    Updating
    Creating google_Colab.py file,
    Updating Name of TestingPandas file,
    Adding pluggin of Csv-Editor

---
+++ google_Colab.py
@@ -0,0 +1,19 @@
+import  pandas as pd
+
+Author = ["Sun Tzu", "Fernando Pessoa", "Thomas Mann", "Joao Guimaraes Rosa"]
+Book = ["A Arte da Guerra", "Poesias Seleccionadas", "A montanha Mágica", "Primeiras Estórias"]
+Year = [2000, 2004, 2015, 2017]
+
+database = {
+    "Author":Author,
+    "Book":Book,
+    "Year":Year
+}
+
+authors = pd.DataFrame(database)
+print(authors, "\n", type(authors))
+
+df = pd.DataFrame(authors)
+print(df)
+
+df.to_csv("authors.csv")
